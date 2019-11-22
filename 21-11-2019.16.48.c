9fd449a68bb0f0377c9417c9bc04ba34f0d67772
ririnzein <57787339+ririnzein@users.noreply.github.com>
2019-11-22 16:48:49 +0700

    Initial commit

---
+++ 21-11-2019.16.48.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+    int i = 0;
+    while (i < 30)
+    {
+        printf ("Hello Ririn!\n");
+        i++;
+    }
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# While-Loop
+ Belajar While Loop
